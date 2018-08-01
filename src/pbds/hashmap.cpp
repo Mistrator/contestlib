bc9c7ec0c0d30982f24f20047568aed5a31c9ee7
Miska Kananen <miska.kananen@gmail.com>
2018-08-01 16:34:42 +0300

    Add PBDS hashmap

---
+++ src/pbds/hashmap.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// get a random number
+uint32_t rd() {
+	uint32_t ret;
+	asm volatile("rdrand %0" :"=a"(ret) ::"cc");
+	return ret;
+}
+
+const uint32_t XR = rd();
+
+// xor with a random number to avoid anti-hash tests
+struct chash {
+    int operator()(int x) const { return hash<int>{}(x^XR); }
+};
+
+int n;
+gp_hash_table<int, int, chash> s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		int x;
+		cin >> x;
+		s[x] = 1;
+	}
+	cout << s.size() << "\n";
+	return 0;
+}
