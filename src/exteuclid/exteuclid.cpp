84a8012e2f6ac640299d6c54dd1de86b920d47dc
Miska Kananen <miska.kananen@gmail.com>
2019-05-24 16:40:47 +0300

    Add exteuclid and linear sieve

--- src/exteuclid/exteuclid.cpp
+++ src/exteuclid/exteuclid.cpp
@@ -1,6 +1,7 @@
 // find pair (x, y) that satisfies ax + by = gcd(a, b)
 // such pair always exists
 // if there are multiple solutions, finds the one with x <= y and |x|+|y| minimal
+// not tested properly
 int exteuclid(int a, int b, int &x, int &y) {
 	if (b == 0) {
 		x = 1;
